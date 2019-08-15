3aa48e5e4e9b0381ba1d98f38eaca82d0553c175
insanefury <justdarsan@gmail.com>
2019-08-15 14:15:18 -0300

    Added Basic Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform crosshair;
+    public float speed =  5;
+    public float rayLenght = 100;
+
+    Vector3 pointToLook = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+
+        RotateToMouse();
+    }
+
+
+    void RotateToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up,Vector3.zero);
+
+        if (groundPlane.Raycast(ray,out rayLenght))
+        {
+            pointToLook = ray.GetPoint(rayLenght);
+            Debug.DrawLine(ray.origin, pointToLook, Color.blue);
+        }
+
+        Vector3 forward = new Vector3(pointToLook.x,transform.position.y,pointToLook.z) - transform.position;
+
+        crosshair.position = pointToLook;
+
+        transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+    }
+}
