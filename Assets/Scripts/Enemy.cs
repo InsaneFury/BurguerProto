476c8ae29dfd900d4b9f331cc0b109590ec893ae
insanefury <justdarsan@gmail.com>
2019-08-29 12:22:08 -0300

    Added follow enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public float damage = 1;
+    public NavMeshAgent enemyAgent;
+    Player player;
+    private void Start()
+    {
+        player = Player.Get();
+        enemyAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        enemyAgent.SetDestination(player.transform.position);
+    }
 }
