8ed3de7fbf57fb02371b0cc4c16b817336440145
insanefury <justdarsan@gmail.com>
2019-09-08 15:04:35 -0300

    Fixed enemy physics

--- Assets/Scripts/Character/Explosion.cs
+++ Assets/Scripts/Character/Explosion.cs
@@ -36,4 +36,10 @@ public class Explosion : MonoBehaviour
         }
         Destroy(gameObject);
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, explosionRadius);
+    }
 }
