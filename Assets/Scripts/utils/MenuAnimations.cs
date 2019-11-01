48085aa19d24b36f8575347f45899884a631b49d
insanefury <justdarsan@gmail.com>
2019-11-01 04:40:49 -0300

    Fixed difficulty buttons

---
+++ Assets/Scripts/utils/MenuAnimations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimations : MonoBehaviour
+{
+    public GameObject difficulties;
+    public GameObject init;
+
+    public void DeactiveDifficulties()
+    {
+        difficulties.SetActive(false);
+    }
+
+    public void ActiveDifficulties()
+    {
+        difficulties.SetActive(true);
+    }
+
+    public void DeactiveInit()
+    {
+        init.SetActive(false);
+    }
+
+    public void ActiveInit()
+    {
+        init.SetActive(true);
+    }
+}
