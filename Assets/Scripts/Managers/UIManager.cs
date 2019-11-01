68bffc3b9b513d2a8064fc9e7f1a6dceac4ab2c6
insanefury <justdarsan@gmail.com>
2019-11-01 13:11:53 -0300

    added mini assets & fiixed minor bugs

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -69,6 +69,8 @@ public class UIManager : MonobehaviourSingleton<UIManager>
         RefreshSkillsIcons();
         RefreshWaveInfo();
         RefreshStats();
+        RefreshHealthbar();
+        RefreshSouls();
     }
 
     public void ActiveInGameUI()
