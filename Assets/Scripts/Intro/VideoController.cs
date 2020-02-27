c2831af637739c7dbd7b4bc1cf874dd42af171e8
Ivan <justdarsan@gmail.com>
2020-02-27 02:20:26 -0300

    Added intro video and skip button

---
+++ Assets/Scripts/Intro/VideoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer vid;
+
+    void Start() {
+        vid.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Skip()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+}
