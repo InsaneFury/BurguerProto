74cd2001063eb3a7a399d29e2ff0baf4f89cd07a
InsaneFury <justdarsan@gmail.com>
2020-07-23 09:01:29 -0300

    Parallax Refactorized

--- Assets/Scripts/utils/Parallax.cs
+++ Assets/Scripts/utils/Parallax.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-
+    [Header("Settings")]
     public float Margin;
     public float Layer;
-    float x;
-    float y;
-    float Easing = 0.2f;
-    Vector3 pos;
-    void Start()
-    {
-        pos = transform.position;
-    }
+    public float Easing = 0.8f;
 
+    [Space]
+    [SerializeField] float x;
+    [SerializeField] float y;
+    [SerializeField] Vector2 parallaxPosition;
+    [SerializeField] Vector3 pos;
+    void Start() => pos = transform.position;
+    
     void Update()
     {
-        float targetX = Input.mousePosition.x - Screen.width / 2f;
-        float dx = targetX - x;
-        x += dx * Easing;
-        float targetY = Input.mousePosition.y - Screen.height / 2f;
-        float dy = targetY - y;
-        y += dy * Easing;
+        DoParallaxVFX();
+    }
+
+    void DoParallaxVFX()
+    {
+        parallaxPosition = new Vector2((Input.mousePosition.x - Screen.width / 2f) - x, (Input.mousePosition.y - Screen.height / 2f) - y);
+
+        x += parallaxPosition.x * Easing * Time.deltaTime;
+        y += parallaxPosition.y * Easing * Time.deltaTime;
+
         Vector3 direction = new Vector3(x, y, 0f);
         Vector3 depth = new Vector3(0f, 0f, Layer);
         transform.position = pos - direction / 500f * Margin + depth;
