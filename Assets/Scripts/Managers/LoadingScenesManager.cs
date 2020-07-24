95036fd416312e67534d9e56e4e261149b78a54f
InsaneFury <justdarsan@gmail.com>
2020-07-24 00:54:26 -0300

    Added LeanTween & Loading fake time, tips

--- Assets/Scripts/Managers/LoadingScenesManager.cs
+++ Assets/Scripts/Managers/LoadingScenesManager.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,6 +10,11 @@ public partial class LoadingScenesManager : MonobehaviourSingleton<LoadingScenes
 {
     public GameObject loadingScreen;
     public Image progressBar;
+    public TextMeshProUGUI tipsText;
+    public CanvasGroup alphaCanvas;
+    public string[] tips;
+    public float tipsIntervaleTime = 1f;
+    public float fakeWaitLoadingTime = 3f;
 
     private List<AsyncOperation> scenesLoading = new List<AsyncOperation>();
     private float totalSceneProgress;
@@ -20,14 +27,11 @@ public partial class LoadingScenesManager : MonobehaviourSingleton<LoadingScenes
         SceneManager.LoadSceneAsync((int)SceneIndexes.MENU, LoadSceneMode.Additive);
     }
 
-    void Update()
-    {
-        
-    }
-
     public void LoadGame()
     {
         loadingScreen.gameObject.SetActive(true);
+        StartCoroutine(GenerateTips());
+
         scenesLoading.Add(SceneManager.UnloadSceneAsync((int)SceneIndexes.MENU));
         scenesLoading.Add(SceneManager.LoadSceneAsync((int)SceneIndexes.LEVEL_1, LoadSceneMode.Additive));
 
@@ -52,6 +56,30 @@ public partial class LoadingScenesManager : MonobehaviourSingleton<LoadingScenes
                 yield return null;
             }
         }
+
+        yield return new WaitForSeconds(fakeWaitLoadingTime);
         loadingScreen.gameObject.SetActive(false);
+    }
+
+    public int tipCount;
+    public IEnumerator GenerateTips()
+    {
+        tipCount = Random.Range(0, tips.Length);
+        tipsText.text = tips[tipCount];
+        while (loadingScreen.activeInHierarchy)
+        {
+            yield return new WaitForSeconds(tipsIntervaleTime);
+            LeanTween.alphaCanvas(alphaCanvas, 0, 0.5f);
+
+            yield return new WaitForSeconds(0.5f);
+
+            tipCount++;
+            if (tipCount >= tips.Length)
+                tipCount = 0;
+
+            tipsText.text = tips[tipCount];
+
+            LeanTween.alphaCanvas(alphaCanvas, 1.0f, 0.5f);
+        }
     }
 }
