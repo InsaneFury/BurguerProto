ed138f815754ef92122ddd849b9762d1822d0067
InsaneFury <justdarsan@gmail.com>
2020-07-28 12:55:21 -0300

    Added quit funcitonality

--- Assets/Scripts/utils/MenuHandler.cs
+++ Assets/Scripts/utils/MenuHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MenuHandler : MonoBehaviour
@@ -7,20 +8,25 @@ public class MenuHandler : MonoBehaviour
     public GameObject[] settingsMenus;
 
     private void Start() => ResetAll();
-
     public void ActiveMenu(int activeMenu) => SetAllSettingsMenus(activeMenu);
-
     public void ResetAll() => SetAllSettingsMenus(0, true);
-
-
-    private void SetAllSettingsMenus(int activeMenu = 0,bool hideAll = false)
+    private void SetAllSettingsMenus(int activeMenu = 0, bool hideAll = false)
     {
         for (int i = 0; i < settingsMenus.Length; i++)
         {
             settingsMenus[i].SetActive(false);
-            if((i == activeMenu) && !hideAll)
+            if ((i == activeMenu) && !hideAll)
                 settingsMenus[i].SetActive(true);
         }
     }
 
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+    Application.Quit();
+#endif
+    }
 }
